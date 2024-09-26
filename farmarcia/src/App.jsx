d3304b7647b95b96a46a15729cfe0577cb496892
vithorLorramValente7 <vithor_valente@estudante.sesisenai.org.br>
2024-09-26 17:18:50 -0300

    desenvolvemos um site para a farmarcia

---
+++ farmarcia/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import FarmarciaNavBar from './components/FarmarciaNavBar'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   <FarmarciaNavBar/>
+
+
+
+
+    </>
+  )
+}
+
+export default App
---
+++ farmarcia/src/components/FarmarciaNavBar.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import "./NavBar.css"
+
+
+
+
+function FarmarciaNavBar() {
+  return (
+    <div>
+     <div className='nav_bar_container'>
+      
+      <img src="/images/LogoImagemL.png" alt="" />
+      
+      <input type="text" className='input_pesquisa'/>
+        
+        <button className='button_farmarcia'>politica da empresa</button>
+        <button className='button_farmarcia'>roupas</button>
+        <button className='button_farmarcia'>carinho🚗</button>
+        
+        
+        </div> 
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default FarmarciaNavBar
